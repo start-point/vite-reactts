feef69170e135982f86d7840e986a9a1d696c6bc
suki <echo_C120@163.com>
2022-02-05 23:41:13 +0800

    add

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,7 +8,12 @@ export interface IHomeProps {
 const Home = (props: IHomeProps) => {
   console.log(props, 'props');
 
-  return <button>点我</button>;
+  const clickAction = () => {
+    const { history } = props;
+    history.push('/vite');
+  };
+
+  return <button onClick={clickAction}>点我进入vite页面</button>;
 };
 
 export default connect(({ vite }: { vite: string }) => vite)(Home);
--- src/pages/Vite/index.tsx
+++ src/pages/Vite/index.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 
-interface IViteProps {}
+interface IViteProps {
+  history: any;
+}
 
-const Vite: React.FunctionComponent<IViteProps> = () => {
+const Vite: React.FunctionComponent<IViteProps> = (props) => {
+  const clickAction = () => {
+    const { history } = props;
+    history.push('/');
+  };
   return (
     <>
       <h1>我是vite页面</h1>
+      <button onClick={clickAction}>点我进入home页面</button>
     </>
   );
 };
